5343d551036315777da1927832b5c7302c56bd14
AhmadFrazDEV <chahmad2004@gmail.com>
2023-01-09 14:46:18 +0500

    SignIn_SignUp

---
+++ SignIn_SignUp/BL/MuserBL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignIn_SignUp.BL
+{
+    public class Muser
+    {
+        public string name;
+    }
+}
---
+++ SignIn_SignUp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SignIn_SignUp.BL;
+
+
+namespace SignIn_SignUp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            panel2.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox2.Checked == true)
+            {
+                panel2.Visible = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string paswd = textBox2.Text;
+            
+        }
+    }
+}
